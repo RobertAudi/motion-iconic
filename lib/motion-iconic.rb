2abe3789543d974885eac7a3f3461a06880683b4
Aziz Light <aziz@azizlight.me>
2014-01-24 14:59:38 +0100

    Add the FontAwesomeKit dependency and a sample project

--- lib/motion-iconic.rb
+++ lib/motion-iconic.rb
@@ -2,5 +2,12 @@ unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
+require "motion-cocoapods"
+
 Motion::Project::App.setup do |app|
+  app.files += Dir.glob(File.join(File.dirname(__FILE__), "motion-iconic", "*.rb"))
+
+  app.pods do
+    pod "FontAwesomeKit", "~> 2.1.0"
+  end
 end
